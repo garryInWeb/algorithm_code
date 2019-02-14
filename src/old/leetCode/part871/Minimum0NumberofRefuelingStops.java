fd79e10cfa24993facbb61fa19b52dff152d3b5b
zhengtengfei <zhengtengfei@tuandai.com>
2019-02-14 18:05:57 +0800

    part871 Minimun Number Of Refueling Stops Part1

---
+++ src/old/leetCode/part871/Minimum0NumberofRefuelingStops.java
@@ -0,0 +1,21 @@
+package old.leetCode.part871;
+
+/**
+ * Created by zhengtengfei on 2019/2/14.
+ */
+public class Minimum0NumberofRefuelingStops {
+    int count = 0;
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        if (startFuel > target)
+            return count;
+        int[][] sortSations = sortStations(stations);
+    }
+
+    private int[][] sortStations(int[][] stations) {
+        int[][] result = stations;
+
+
+        return stations;
+    }
+
+}
