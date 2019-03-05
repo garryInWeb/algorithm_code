9c85ee0ae296c89a6c9235a0a1534349de373001
zhengtengfei <zhengtengfei@tuandai.com>
2019-03-05 18:06:47 +0800

    part871 finish

--- src/old/leetCode/part871/Minimum0NumberofRefuelingStops.java
+++ src/old/leetCode/part871/Minimum0NumberofRefuelingStops.java
@@ -6,70 +6,20 @@ import java.util.Arrays;
  * Created by zhengtengfei on 2019/2/14.
  */
 public class Minimum0NumberofRefuelingStops {
-    int count = 0;
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
-        if (startFuel > target)
-            return count;
-        int[][] sortSations = sortStations(stations,0,stations.length-1);
-        int count = 0;
-        int i = 0;
-        int goS = 0;
-        while(target-startFuel > 0){
-            if (sortSations.length == 0) return -1;
-            if (i == sortSations.length) return -1;
-            if (startFuel + goS >= sortSations[i][0]){
-                target -= sortSations[i][0];
-                startFuel = startFuel - sortSations[i][0] + sortSations[i][1];
-                goS += sortSations[i][0];
-                sortSations = buildNewSations(sortSations,sortSations[i][0]);
-                i = 0;
-                count ++;
-                continue;
-            }
-            i++;
-        }
-        return count;
-    }
-
-    private int[][] buildNewSations(int[][] sortSations,int stations) {
-        int count = 0;
-        for (int i = 0; i < sortSations.length; i++){
-            if  (sortSations[i][0] > stations) count++;
-        }
-        int[][] result = new int[count][2];
-        int j = 0;
-        for (int i = 0; i < sortSations.length; i++){
-            if  (sortSations[i][0] > stations) {
-                result[j] = sortSations[i];
-                j++;
+        int[] dp = new int[stations.length + 1];
+        dp[0] = startFuel;
+        for (int i = 0; i < stations.length; i++){
+            for (int t = i; t >= 0 ; t--){
+                if (dp[t] >= stations[i][0])
+                    dp[t+1] = Math.max(dp[t+1],dp[t] + stations[i][1]);
             }
         }
-        return result;
-    }
-
-    public int[][] sortStations(int[][] stations,int offset,int last) {
-
-        if (offset >= last) return stations;
-        int j = offset;
-        int partition = stations[last][1];
-        for (int i = offset; i < last; i++){
-            if (stations[i][1] > partition){
-                swap(stations,j,i);
-                j++;
-            }
+        for (int i = 0; i < dp.length; i++){
+            if (dp[i] >= target)
+                return i;
         }
-        swap(stations,last,j);
-
-        sortStations(stations,offset,j-1);
-        sortStations(stations,j+1,last);
-
-        return stations;
-    }
-
-    private void swap(int[][] stations, int j, int i) {
-        int[] temp = stations[j];
-        stations[j] = stations[i];
-        stations[i] = temp;
+        return -1;
     }
 
     public static void main(String[] args) {
